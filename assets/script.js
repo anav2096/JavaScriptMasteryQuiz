f2c3fe1354778a21ac1607e2f3995097eb96472a
anav <anav2096@gmail.com>
2022-01-21 21:52:52 -0500

    initialCommit

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+//Need centered interface that presents question and lists of answers
+//Start button starts timer - timer should be housed in a statement about the quiz
+//Questions appear one after the other, alert right or wrong answer
+//Wrong answers get time deducted
+//End when all questions anwswered or timer = 0
+//Must log high scored and save initials - make list accessible from main screen
+//Will need to create questions, answers, true-false logic, true counter,
+//true counter log that stores info and moves high score to top of list
+
+var creatQuizArea = function(){
+
+document.createElement("div");
+}
+
+<button class="btn" id = "startButton">Start Quiz</button>
